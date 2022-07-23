3e7e7046536028edfff1f7bc91cdbb4a4cad65ed
julo <stasiakjulian@gmail.com>
2022-07-23 05:43:51 +0200

    przed ruthless mode

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 import numpy as np
 import keyboard
 
-print(r"""\
+print(r"""
 
            ___ _    _      _             _      _     
   ___ __ _| __(_)__| |_   | |__ _  _    (_)_  _| |___ 
@@ -37,6 +37,11 @@ def pause_after(pause_time):
             time.sleep(5)
             raise PauseWhenCaught
 
+print("Umieść kursor troszkę nad głową postaci (tam gdzie pojawia się emotka ryby) i wciśnij lewy ctrl.")
+keyboard.wait('left ctrl')
+x, y = pyautogui.position()
+ssregion = (x-45, y-45, 90, 90)
+#old region = (820,315,90,90)
 
 while True:
     try:
@@ -45,17 +50,17 @@ while True:
 
         time.sleep(1.5)
 
-        ss = pyautogui.screenshot('ss.png', region=(820,315,90,90))
+        ss = pyautogui.screenshot('ss.png', region=ssregion)
         zdj1 = cv2.imread('ss.png')
 
 
         while True:
             try:
-                ss2 = pyautogui.screenshot('ss2.png', region=(820,315,90,90))
+                ss2 = pyautogui.screenshot('ss2.png', region=ssregion)
                 zdj2 = cv2.imread('ss2.png')
                 m = round(mse(zdj1, zdj2))
                 print(f'MSE: {m}', end='\r')
-                time.sleep(0.1)
+                time.sleep(0.07)
 
                 if keyboard.is_pressed(pause_button):
                     while keyboard.is_pressed(pause_button):
@@ -65,11 +70,19 @@ while True:
                     time.sleep(5)
                     break
 
-                if m > 9000:
+                if m > 11000:
                     print("\nWykryto rybę!")
-                    delay = random.randrange(1, 4, 1)/10 + random.randrange(0, 100, 1)/1000
+                    time_lottery = random.randrange(1,20,1)
+
+                    if time_lottery == 7:
+                        delay = random.randrange(4, 6, 1)/10 + random.randrange(0, 100, 1)/1000
+                    else:
+                        #delay = random.randrange(160, 330, 1)/1000
+                        delay = random.randrange(10, 20, 1)/1000
+
                     buffer = random.randrange(1, 4, 1)/2 + random.randrange(0, 99, 1)/1000
                     print(f'Wyławiam z opóźnieniem: {delay}')
+                    pyautogui.click(button='right', x=x, y=y)
                     time.sleep(delay)
                     #wciska spacje na 0.1s
                     keyboard.press(57)
@@ -90,7 +103,7 @@ while True:
                     
                     pause_after(3.5)
 
-                    ss = pyautogui.screenshot('ss.png', region=(820,315,90,90))
+                    ss = pyautogui.screenshot('ss.png', region=ssregion)
                     zdj1 = cv2.imread('ss.png')
             except PauseWhenCaught:
                 break
